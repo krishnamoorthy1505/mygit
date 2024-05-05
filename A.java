cccbace46417ae9a039313b58ccf6959ff19c2a3
krishnamoorthy1505 <krishnamoorthy82@gmail.com>
2024-05-05 10:07:22 +0530

    merge conflict on main branch

--- A.java
+++ A.java
@@ -1,7 +1,7 @@
 public class A {
-    private int a;
-    private int b;
-    private int c;
-    private int d;
+    private int e;
+    private int f;
+    private int g;
+    private int h;
     
 }
